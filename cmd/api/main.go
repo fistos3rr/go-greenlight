a8d97bda12ce07c317b922e01a1d86c6b2e8e8f3
fistos3rr <fistos3rr@gmail.com>
2026-07-30 15:14:45 +0300

    cors: simple cors middleware

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"flag"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -37,6 +38,9 @@ type config struct {
 		username string
 		password string
 		sender   string
+	}
+	cors struct {
+		trustedOrigins []string
 	}
 }
 
@@ -77,6 +81,11 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "maxie.smitham", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "gWT4CaaNU6fq4SUmnp", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.com>", "SMTP sender")
+
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	}
 
 	flag.Parse()
 
